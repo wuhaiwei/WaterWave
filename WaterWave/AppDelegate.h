98161079edfa7d71eee0935484bc6b9e4d4ec02b
wuhaiwei <wuhaiwei@qding.me>
2018-08-07 17:47:18 +0800

    Initial Commit

---
+++ WaterWave/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WaterWave
+//
+//  Created by hu on 2018/8/7.
+//  Copyright © 2018年 wu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WaterWave/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WaterWave
+//
+//  Created by hu on 2018/8/7.
+//  Copyright © 2018年 wu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
